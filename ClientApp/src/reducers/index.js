39791430cc5d8920e9cd6e2b7e5f095715e42a86
Dean Agan <agandfr@gmail.com>
2020-02-08 10:55:51 +1100

    Create root reducer

---
+++ ClientApp/src/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+import tripReducers from './tripReducers';
+
+
+const rootReducer = combineReducers({
+    trips: tripReducers
+});
+
+
+export default rootReducer;
