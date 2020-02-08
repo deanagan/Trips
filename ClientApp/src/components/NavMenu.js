0399de581bd8a22e3f7385824843932358d0608f
Dean Agan <agandfr@gmail.com>
2020-02-08 16:15:02 +1100

    Change NavMenu into function based and use auth0

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -3,44 +3,40 @@ import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLi
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+import {useAuth0} from '../auth0-wrapper';
 
-  constructor (props) {
-    super(props);
+const NavMenu = () => {
+  const {isAuthenticated, loginWithRedirect, logout} = useAuth0();
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Container>
+          <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
+          {isAuthenticated ? (
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
+              </NavItem>
+              <NavItem>
+                <button className="btn btn-danger" onClick={() => logout()}>Log out</button>
+              </NavItem>
+            </ul>
+          ) : (
               <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
-                </NavItem>
+              <NavItem>
+                <button className="btn btn-success" onClick={() => loginWithRedirect()}>Log In</button>
+              </NavItem>
               </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+          )}
+    
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
+
+export default NavMenu;
